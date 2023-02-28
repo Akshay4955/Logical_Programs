801411b7743a95ccd0f73ac3684994684000e0bb
Akshay4955 <akshayshedge11@gmail.com>
2023-02-28 08:55:27 +0530

    Added: code to check perfect number

--- src/main/java/com/bridgelabz/logical_programs/LogicalProblems.java
+++ src/main/java/com/bridgelabz/logical_programs/LogicalProblems.java
@@ -8,6 +8,24 @@ public class LogicalProblems {
     public static void main(String[] args) {
         LogicalProblems logicalProblems = new LogicalProblems();
         logicalProblems.getFibonacciSeries();
+        logicalProblems.getPerfectNumber();
+    }
+
+    void getPerfectNumber() {
+        System.out.println("Welcome to get perfect number");
+        System.out.println("Enter number of your choice");
+        int number = input.nextInt();
+        int total = 0;
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                total = total + i;
+            }
+        }
+
+        if (total == number)
+            System.out.println(number + " is a perfect number");
+        else
+            System.out.println(number + " is not a perfect number");
     }
 
     public void getFibonacciSeries() {
